173a290eeb7cc7dfe9a862bde2bf7c493d4f9549
Chandaluri Vamsi Krishna <cvamsik99@gmail.com>
2024-01-23 17:59:34 +0530

    feat: Implemented Page6
    Signed-off-by: Chandaluri Vamsi Krishna <cvamsik99@gmail.com>

--- src/components/page6/Page6.tsx
+++ src/components/page6/Page6.tsx
@@ -1,3 +1,44 @@
+import { CheckCircle2 } from "lucide-react";
+import P6i1 from "../../../public/images/P6i1.png";
+import Image from "next/image";
+
 export const Page6 = () => {
-  return <></>;
+  return (
+    <div className="grid grid-cols-2 mt-40 ml-24">
+      <div className="flex flex-col gap-10 m-16">
+        <div className="text-4xl font-bold">
+          Ready to clear the path to perfect communication?
+        </div>
+        <div className="flex flex-col mx-4 gap-4">
+          <div className="flex">
+            <CheckCircle2 className="text-blue-500" />
+            <div className="ml-2 text-gray-500">30 days free trial</div>
+          </div>
+          <div className="flex">
+            <CheckCircle2 className="text-blue-500" />
+            <div className="ml-2 text-gray-500">Cancel at any time</div>
+          </div>
+          <div className="flex">
+            <CheckCircle2 className="text-blue-500" />
+            <div className="ml-2 text-gray-500">Access to all features</div>
+          </div>
+          <div className="flex">
+            <CheckCircle2 className="text-blue-500" />
+            <div className="ml-2 text-gray-500">Peronalized onboarding</div>
+          </div>
+        </div>
+        <div className="flex gap-2 items-center font-medium text-xs">
+          <div className="bg-white py-3 px-4 border-2 rounded-full">
+            Talk to sales
+          </div>
+          <div className="bg-blue-600 text-white py-3 px-4 border-2 rounded-full">
+            Start your free trial
+          </div>
+        </div>
+      </div>
+      <div className="border-l-8 border-t-8 border-black rounded-tl-2xl">
+        <Image src={P6i1} alt="R" />
+      </div>
+    </div>
+  );
 };
